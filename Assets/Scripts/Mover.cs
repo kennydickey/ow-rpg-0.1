22386e0037c7abcf2a74f82471e9b255f28b8356
Ken Dickey <seatacken@gmail.com>
2021-06-12 17:50:20 -0700

    Player movement to click point with raycast

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,32 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] float playerSpeed;
-
-    Ray lastRay;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {
-
-        GetComponent<NavMeshAgent>().destination = target.position;
-        GetComponent<NavMeshAgent>().speed = playerSpeed;
+    {      
 
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+
     }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100); // just a visualization
+        RaycastHit hit; // << informatuion from hit event stored here
+        bool hashit = Physics.Raycast(ray, out hit); // storing in hit, information about where the ray hit, which can be one of multiple ptoperties such as point
+        if (hashit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        //if() //if something happens move player to a fixed target
+        //{
+        //    GetComponent<NavMeshAgent>().destination = target.position;
+        //}
+    }
+
+
+
+
 }
