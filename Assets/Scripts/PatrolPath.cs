012993f2546a1579c10ce9a2fd7c514b02ce0a91
Ken Dickey <seatacken@gmail.com>
2021-06-29 20:25:42 -0700

    Patrol path line gizmos

--- Assets/Scripts/PatrolPath.cs
+++ Assets/Scripts/PatrolPath.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,32 @@ namespace RPG.Control
 
         private void OnDrawGizmos()
         {
-            for (int i = 0; i < transform.childCount; i++) // for children pf Patrol Path
+            for (int i = 0; i < transform.childCount; i++) // for children of Patrol Path
             {
+                
+                int j = GetNextIndex(i);
+                
                 Gizmos.color = Color.cyan;
                 Gizmos.DrawSphere(transform.GetChild(i).position, waypointNodeSize);
+
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j)); // from 0 to 1, from 3 to 0 etc
+                
+
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i+1 == transform.childCount) // next waypoint is the last, index[3] + 1 is 4th
+            {
+                return 0; // draw line from waypoint to 0
+            } // else..
+            return i + 1; // draw line from i to i + 1
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
