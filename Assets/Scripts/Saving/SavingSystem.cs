9b46463b8429f2ad428feed7bdcda1969056264b
Ken Dickey <seatacken@gmail.com>
2021-07-21 23:20:30 -0700

    Saving and loading position using serialization

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -17,12 +18,16 @@ namespace RPG.Saving
 
             using(FileStream stream = File.Open(path, FileMode.Create))
             {
-                stream.WriteByte(102); // decimal value of 102, f according to ascii table, saves to file
-                stream.WriteByte(0xc3); // Hex example, starts with 0x, prints a stylized A..
-                stream.WriteByte(0x80); // .. according to utf8 table
+                //stream.WriteByte(102); // decimal value of 102, f according to ascii table, saves to file
+                //stream.WriteByte(0xc3); // Hex example, starts with 0x, prints a stylized A..
+                //stream.WriteByte(0x80); // .. according to utf8 table
                                         // Alternatively..
-                byte[] byteArray = Encoding.UTF8.GetBytes("¡ yeah um µ");
-                stream.Write(byteArray, 0, byteArray.Length);
+                Transform playerTransform = GetPlayerTransform();
+                // then serialize playerTransform..
+                byte[] positionBuffer = SerializeVector(playerTransform.position);
+                //byte[] byteArray = Encoding.UTF8.GetBytes("¡ yeah um µ");
+                stream.Write(positionBuffer, 0, positionBuffer.Length);
+                //stream.Write(byteArray, 0, byteArray.Length);
                 // auto closes stream on exit of using
             }
             // must do this to close any file handles that were created vv
@@ -35,10 +40,38 @@ namespace RPG.Saving
             print("loading " + path);
             using (FileStream stream = File.Open(path, FileMode.Open)) // bc .Create overwrites file
             {
-                byte[] buffer = new byte[stream.Length]; // create a buffer and specify it's size
-                stream.Read(buffer, 0, buffer.Length);
-                print(Encoding.UTF8.GetString(buffer));
-            }             
+                byte[] loadBuffer = new byte[stream.Length]; // create a buffer and specify it's size
+                stream.Read(loadBuffer, 0, loadBuffer.Length);
+                print(Encoding.UTF8.GetString(loadBuffer));
+
+                Transform playerTransform = GetPlayerTransform();
+                playerTransform.position = DeserializeVector(loadBuffer);
+            }
+        }
+
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        private byte[] SerializeVector(Vector3 vector3) // produce byte array from a vector3
+        {
+            // takes in a Vector3 and converts into a byte array
+            byte[] byteVectors = new byte[3 * 4]; // 3 floats * float size in bytes
+            BitConverter.GetBytes(vector3.x).CopyTo(byteVectors, 0); //byteArray
+            BitConverter.GetBytes(vector3.y).CopyTo(byteVectors, 4); //byteArray
+            BitConverter.GetBytes(vector3.z).CopyTo(byteVectors, 8); //byteArray
+            return byteVectors;
+        }
+
+        private Vector3 DeserializeVector(byte[] bufferArray) // produce our Vector3 from buffer
+        {
+            Vector3 resultVector3 = new Vector3();
+            resultVector3.x = BitConverter.ToSingle(bufferArray, 0); //convert one byte(4 chars) starting from 0
+            resultVector3.y = BitConverter.ToSingle(bufferArray, 4);
+            resultVector3.z = BitConverter.ToSingle(bufferArray, 8);
+            return resultVector3;
         }
 
         private string GetPathFromFile(string saveFile)
