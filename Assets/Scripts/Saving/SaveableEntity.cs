12e5be825b4b2247d0f3b363107746bbcd1dbfd9
Ken Dickey <seatacken@gmail.com>
2021-07-24 22:26:23 -0700

    Allow for Uid update in scene file only

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways] // allows execution of fle when editing, so executes when we click or whatever in editor window
     public class SaveableEntity : MonoBehaviour
     {
+        // SerializeField persists across reloads
+        // uid needs to be saved in scene file, not in prefab file as it will create duplicate instance uids
+        [SerializeField] string uniqueIdentifier = "";
+
+        private void Update()
+        {
+            // don't allow execution if..
+            if (Application.IsPlaying(gameObject)) return; // checking to see if scene that this.gameObject is in is playing
+
+        }
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object EntityCaptureState()
@@ -21,5 +33,9 @@ namespace RPG.Saving
         {
             print("Restoring state for uid" + GetUniqueIdentifier());
         }
+
+        
+
     }
+
 }
