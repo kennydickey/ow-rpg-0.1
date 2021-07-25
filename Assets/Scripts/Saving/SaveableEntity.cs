728c07cebe4d309e977f171d2c8778a2fa602e3e
Ken Dickey <seatacken@gmail.com>
2021-07-25 11:22:45 -0700

    Generating uids in scen file without generating for prefabs

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace RPG.Saving
 {
     [ExecuteAlways] // allows execution of fle when editing, so executes when we click or whatever in editor window
     public class SaveableEntity : MonoBehaviour
     {
-        // SerializeField persists across reloads
-        // uid needs to be saved in scene file, not in prefab file as it will create duplicate instance uids
+        // SerializeField persists in scene / scene file across reloads, but not across scenes
+        // uid needs to be saved in scene/ scene file, not in prefab file as it will create duplicate instance uids
         [SerializeField] string uniqueIdentifier = "";
 
+        // following code only needed to initially generate uids while creating
+#if UNITY_EDITOR // to avoid error, as this code is not available to package, code is removed entirely when packaging
         private void Update()
         {
             // don't allow execution if..
             if (Application.IsPlaying(gameObject)) return; // checking to see if scene that this.gameObject is in is playing
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return; // empty path returned means we are in a prefab, so don't add uid
 
+            SerializedObject serializedObject = new SerializedObject(this); // gives the serialization of this monobehaviour
+            SerializedProperty idProperty = serializedObject.FindProperty("uniqueIdentifier"); // now we can access it's properties
+            if(string.IsNullOrEmpty(idProperty.stringValue))
+            {
+                idProperty.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties(); // to update or "tell" Unity about the change
+            }
         }
-
+#endif
         public string GetUniqueIdentifier()
         {
             return uniqueIdentifier;
@@ -35,7 +46,6 @@ namespace RPG.Saving
         }
 
         
-
     }
 
 }
