1c71d009b5c36f201d364007f29a8246604b5a7f
Ken Dickey <seatacken@gmail.com>
2021-07-31 22:16:41 -0700

    Clean up from previous commit

---
+++ Assets/Scripts/Saving/ISaveable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RPG.Saving
+{
+    public interface ISaveable
+    {
+        object CaptureState(); // just wants to get state of whatever saveble component
+        void RestoreState(object state); // update saveable component with this state
+    }
+}
