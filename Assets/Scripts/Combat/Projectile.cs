a758aec3cad91686c6f6700c1c45ba994a7b49a4
Ken Dickey <seatacken@gmail.com>
2021-08-23 17:25:28 -0700

    Fire magic and reorganization of weapon folder

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,16 +7,34 @@ namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] float arrowSpeed = 1f;
+        [SerializeField] float projectileSpeed = 1f;
         Health projectileTarget = null; // uses RPG.Core
         float projectileDamage = 0; // no damage from projectile as of now, weapon controls amount
+        [SerializeField] bool isHoming;
+        [SerializeField] int projectileDwell = 12;
+
+        private void Start()
+        {
+            transform.LookAt(AimLocation());
+            StartCoroutine(ProjectileDwell());
+        }
 
         // Update is called once per frame
         void Update()
         {
             if (projectileTarget == null) return;
-            transform.LookAt(AimLocation());
-            transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
+
+            if (isHoming && !projectileTarget.IsDead())
+            {
+                transform.LookAt(AimLocation());
+            }
+            transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+        }
+
+        IEnumerator ProjectileDwell()
+        {
+            yield return new WaitForSeconds(projectileDwell);
+            Destroy(gameObject);
         }
 
         public void SetTarget(Health target, float damage)
@@ -38,6 +56,7 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != projectileTarget) return;
+            if (projectileTarget.IsDead()) return;    // arrow continues without giving damage or being destroyed       
             projectileTarget.TakeDamage(projectileDamage); // Takedamage method is on Health component of our target
             Destroy(gameObject);
         }
