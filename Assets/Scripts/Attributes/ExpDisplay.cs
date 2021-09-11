40d0428c790dc575887e7e6cae96a7657fed1260
Ken Dickey <seatacken@gmail.com>
2021-09-10 19:38:00 -0700

    Saving and displaying experience

---
+++ Assets/Scripts/Attributes/ExpDisplay.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Attributes
+{
+    public class ExpDisplay : MonoBehaviour
+    {
+        Experience experience;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", experience.GetExp().ToString());
+        }
+    }
+}
--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.CharaStats;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Attributes
 {
     // Script for the Player. Not using for enemies right now
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0;
 
         public void GainExperience(float addExperience) // called in Health.cs
         {
-            experiencePoints += addExperience;           
+            experiencePoints += addExperience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public float GetExp()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state; // state coming in cast as float to make sure we are getting the right type
         }
     }
 }
