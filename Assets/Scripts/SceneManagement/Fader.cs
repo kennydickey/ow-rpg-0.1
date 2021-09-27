f5e8fcf5462ce67e104a69536dd32afeccb04863
Ken Dickey <seatacken@gmail.com>
2021-09-26 19:21:07 -0700

    Fader fix of problem where fadein and fadeout clash going between scenes as they will try to run concurrently

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -6,6 +6,7 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentActiveFade = null;
 
         private void Awake() // to call ahead of SavingWrapper.Start(), where canvas group does not exist yet
         {
@@ -28,21 +29,31 @@ namespace RPG.SceneManagement
         }
 
         public IEnumerator FadeOut(float time) // total time taken
-        {           
-            while (canvasGroup.alpha < 1)
-            {
-                //increments by framerate divided by total time
-                canvasGroup.alpha += Time.deltaTime / time;
-                yield return null; // makes coroutine wait for 1 frame
-            }
+        {
+            return fade(1, time);
         }
 
         public IEnumerator FadeIn(float time) // total time taken
         {
-            while (canvasGroup.alpha > 0)
+            return fade(0, time);
+        }
+
+        public IEnumerator fade(float targetFadeValue, float time)
+        {
+            if (currentActiveFade != null)
+            {
+                StopCoroutine(currentActiveFade);
+            }
+            currentActiveFade = StartCoroutine(FadeRoutine(targetFadeValue, time));
+            yield return currentActiveFade;
+        }
+
+        private IEnumerator FadeRoutine(float targetFadeValue, float time)
+        {
+            while (!Mathf.Approximately(canvasGroup.alpha, targetFadeValue)) // while fader is not at target value
             {
                 //increments by framerate divided by total time
-                canvasGroup.alpha -= Time.deltaTime / time;
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetFadeValue, Time.deltaTime / time);
                 yield return null; // makes coroutine wait for 1 frame
             }
         }
