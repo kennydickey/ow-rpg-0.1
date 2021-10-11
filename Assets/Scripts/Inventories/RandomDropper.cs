4eff3c28a4b80138255c9c3a9bd91e0d970229f8
Ken Dickey <seatacken@gmail.com>
2021-10-11 15:44:28 -0700

    Random drops from enemy

--- Assets/Scripts/Inventories/RandomDropper.cs
+++ Assets/Scripts/Inventories/RandomDropper.cs
@@ -4,21 +4,42 @@ using GameDevTV.Inventories;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class RandomDropper : ItemDropper // overriding from ItemDropper script rather than monbehaviour
+namespace RPG.Inventories
 {
-    //// CONFIG DATA
-    //[Tooltip("How far pickups can be fall from dropper")]
-    //[SerializeField] float scatterDistance = 1;
+    public class RandomDropper : ItemDropper // overriding from ItemDropper script rather than monbehaviour
+    {
+        // CONFIG DATA
+        [Tooltip("How far pickups can be fall from dropper")]
+        [SerializeField] float scatterDistance = 1;
+        [SerializeField] InventoryItem[] dropLibrary;
+        [SerializeField] int numberOfDrops = 2;
 
-    //// Start is called before the first frame update
-    //protected override Vector3 GetDropLocation() // overrides ItemDropper.GetDropLocation
-    //{
-    //    Vector3 randomPoint = transform.position + (Random.insideUnitSphere * scatterDistance);
-    //    NavMeshHit hit;
-    //    // bool which samples navmesh within these parameters vv
-    //    if (NavMesh.SamplePosition(randomPoint, out hit, 0.1f, NavMesh.AllAreas))
-    //    {
-    //        return hit.position;
-    //    }
-    //}
+        // CONSTANTS
+        const int attempts = 30; // max navmesh sample attempts
+
+        public void RandomDrop()
+        {
+            for (int i = 0; i < attempts; i++)
+            {
+                InventoryItem randomItem = dropLibrary[Random.Range(0, dropLibrary.Length)];
+                DropItem(randomItem, 1); // second number is number to drob and stackabilty
+            }             
+        }
+
+        // Start is called before the first frame update
+        protected override Vector3 GetDropLocation() // overrides ItemDropper.GetDropLocation
+        {
+            for (int i = 0; i < attempts; i++)
+            {
+               Vector3 randomPoint = transform.position + (Random.insideUnitSphere * scatterDistance);
+                NavMeshHit hit;
+                // bool which samples navmesh within these parameters vv
+                if (NavMesh.SamplePosition(randomPoint, out hit, 0.1f, NavMesh.AllAreas))
+                {
+                    return hit.position; // a hit here returns, therefore exits for loop
+                }
+            }
+            return transform.position; // if navmesh sample unsuccessful, just drop here
+        }
+    }
 }
